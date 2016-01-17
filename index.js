7954783cd333e84c13437c0be06ad9e997978fa0
Joe Fleming <joe.fleming@gmail.com>
2016-01-17 11:58:03 -0700

    add logging via winston, example env file

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const path = require('path');
 const express = require('express');
 const ehbs  = require('express-handlebars');
 const routes = require('./routes');
+const mwLogger = require('./middleware/logger');
 
 const app = express();
 const hbs = ehbs.create({
@@ -15,6 +16,7 @@ app.engine('hbs', hbs.engine);
 app.set('view engine', 'hbs');
 app.set('port', (process.env.PORT || 8080));
 
+app.use(mwLogger.access);
 
 app.use(express.static(path.resolve(__dirname, 'public'), {
   index: false,
@@ -26,6 +28,8 @@ app.use('/', routes);
 app.get('*', function (req, res) {
   res.redirect(302, '/');
 });
+
+app.use(mwLogger.error);
 
 app.listen(app.get('port'), function () {
   console.log('Server listening on port', app.get('port'));
---
+++ middleware/logger.js
@@ -0,0 +1,53 @@
+const expressWinston = require('express-winston');
+const winston = require('winston'); // for transports.Console
+
+function addServerTransport(transports) {
+  const config = {
+    host: process.env.WINSTOND_HOST,
+    port: process.env.WINSTOND_PORT,
+    ssl: !!process.env.WINSTOND_SSL,
+    path: process.env.WINSTOND_PATH || '/',
+  };
+
+  // append auth if defined
+  if (process.env.WINSTOND_USER && process.env.WINSTOND_PASS) {
+    config.auth = {
+      username: process.env.WINSTOND_USER,
+      password: process.env.WINSTOND_PASS,
+    };
+  }
+
+  if (config.host && config.port) {
+    transports.push(new (winston.transports.Http)(config));
+  }
+}
+
+exports.access = (function () {
+  const transports = [
+    new (winston.transports.Console)({
+      colorize: true,
+      timestamp: true,
+    }),
+  ];
+
+  addServerTransport(transports);
+
+  return expressWinston.logger({
+    transports: transports
+  });
+})();
+
+exports.error = (function () {
+  const transports = [
+    new (winston.transports.Console)({
+      colorize: true,
+      timestamp: true,
+    }),
+  ];
+
+  addServerTransport(transports);
+
+  return expressWinston.errorLogger({
+    transports: transports
+  });
+})();

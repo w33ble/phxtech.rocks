c974f979a304ba1e96510c74a0face1b70348989
Joe Fleming <joe.fleming@gmail.com>
2016-05-25 13:43:01 -0700

    simplify the routing

--- routes/calendar.js
+++
@@ -1,6 +0,0 @@
-module.exports = function (router) {
-  router.get('/calendar', function(req, res) {
-    res.redirect(302, 'http://nextplex.com/phoenix-az/calendar');
-  });
-};
-
--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,26 @@
+var path = require('path');
 var express = require('express');
+
+var public = path.resolve('..', 'public');
+
 var router = express.Router({
   caseSensitive: true
 });
 
-require('./static')(router);
-require('./calendar')(router);
-require('./home')(router);
+// static assets
+router.use(express.static(public, {
+  index: false,
+  maxAge: '1d'
+}));
+
+// calendar site redirect
+router.get('/calendar', function(req, res) {
+  res.redirect(302, 'http://nextplex.com/phoenix-az/calendar');
+});
+
+// home page route
+router.get('/', function (req, res) {
+  res.render('home');
+});
 
 module.exports = router;

45ef0b597cdea77c7b84da66819ee2e72f08147a
Joe Fleming <joe.fleming@gmail.com>
2016-05-26 11:42:25 -0700

    create an rss feed route

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,6 @@
 var path = require('path');
 var express = require('express');
+var getEvents = require('../lib/get_events');
 
 var public = path.resolve('..', 'public');
 
@@ -16,6 +17,26 @@ router.use(express.static(public, {
 // calendar site redirect
 router.get('/calendar', function(req, res) {
   res.redirect(302, 'http://nextplex.com/phoenix-az/calendar');
+});
+
+// calendar site redirect
+router.get('/event-feed', function(req, res) {
+  getEvents.thisWeek()
+  .then((events) => {
+    var displayEvents = events.map((event) => {
+      event.details = event.address + ' - ' + event.time;
+      event.href = 'http://nextplex.com/' + event.href;
+      return event;
+    })
+
+    res.render('event_feed', {
+      layout: false,
+      events: displayEvents
+    });
+  }).
+  catch((err) => {
+    res.status(500).end();
+  })
 });
 
 // home page route

cc9769ef74908e2dd91901838284be5708165cb5
Joe Fleming <joe.fleming@gmail.com>
2018-04-26 20:01:54 -0700

    feat: working knex sqlite3 example

---
+++ packages/event-api/src/index.mjs
@@ -0,0 +1,32 @@
+import path from 'path';
+import knex from 'knex';
+import Puid from 'puid';
+import initModels from './models/index.mjs';
+
+const puid = new Puid(true);
+
+const db = knex({
+  dialect: 'sqlite3',
+  connection: {
+    filename: 'data/api.db',
+  },
+  useNullAsDefault: true,
+});
+
+async function eventApiServer() {
+  try {
+    await initModels(db);
+    const doc = {
+      id: puid.generate(),
+      name: 'PHX Android',
+      description: `This is a group for anyone interested in Android Development, Design and development of Android Applications. All skill levels are welcome. I started this group to help grow the Android Google Developer Group community and to meet other people interested in Android Development. Come to the meet up to hang out and watch or be part of the conversation. Bring a project you're working on. Anything Android Development is welcome (dev, design, how to market your app, etc).`,
+    };
+    const resp = await db('groups').insert(doc);
+  } catch (err) {
+    console.error(err);
+  }
+
+  return 'done';
+}
+
+export default eventApiServer();
---
+++ packages/event-api/src/models/groups.mjs
@@ -0,0 +1,12 @@
+export default {
+  name: 'groups',
+  // primary_key: 'id', // implied
+  schema: {
+    id: { type: 'string' },
+    name: { type: 'string' }, // not nullable implied
+    description: { type: 'text' },
+    event_type: { type: 'enum', values: ['meetup', 'ical'], default: 'meetup' },
+    website: { type: 'string', nullable: true },
+  },
+  // timestamps: true, // implied
+};

39a6124f56563f845492428a0fb9ced8c814b024
Joe Fleming <joe.fleming@gmail.com>
2018-04-26 20:08:10 -0700

    feat: working example with incrementing id

--- packages/event-api/src/index.mjs
+++ packages/event-api/src/index.mjs
@@ -17,11 +17,12 @@ async function eventApiServer() {
   try {
     await initModels(db);
     const doc = {
-      id: puid.generate(),
+      // id: puid.generate(),
       name: 'PHX Android',
       description: `This is a group for anyone interested in Android Development, Design and development of Android Applications. All skill levels are welcome. I started this group to help grow the Android Google Developer Group community and to meet other people interested in Android Development. Come to the meet up to hang out and watch or be part of the conversation. Bring a project you're working on. Anything Android Development is welcome (dev, design, how to market your app, etc).`,
     };
     const resp = await db('groups').insert(doc);
+    console.log('new groupi %s (%s)', doc.name, resp[0]);
   } catch (err) {
     console.error(err);
   }
--- packages/event-api/src/models/groups.mjs
+++ packages/event-api/src/models/groups.mjs
@@ -2,7 +2,7 @@ export default {
   name: 'groups',
   // primary_key: 'id', // implied
   schema: {
-    id: { type: 'string' },
+    id: { type: 'increments' },
     name: { type: 'string' }, // not nullable implied
     description: { type: 'text' },
     event_type: { type: 'enum', values: ['meetup', 'ical'], default: 'meetup' },

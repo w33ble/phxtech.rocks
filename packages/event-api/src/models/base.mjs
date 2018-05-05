f0264a90b039b9994894bb8c5eeeaf04e3dfec3e
Joe Fleming <joe.fleming@gmail.com>
2018-05-05 14:57:17 -0700

    chore: switch to simple-knex-model

--- packages/event-api/src/models/base.mjs
+++ packages/event-api/src/models/base.mjs
@@ -1,66 +1,15 @@
+import BaseModel from 'simple-knex-model';
 import Puid from 'puid';
-import Ajv from 'ajv';
 import knex from '../libs/knex.mjs';
 
+BaseModel.knex(knex);
+
 const puid = new Puid(false);
-const ajv = new Ajv();
-// const modelRegistery = new Map(); // TODO: mapping relationships
 
-function validateModel(child) {
-  function showError(msg) {
-    throw new Error(`Model failure, ${msg}: ${child.name}`);
-  }
-
-  if (!child.tableName) showError('`tableName` is required');
-}
-
-export default class BaseModel {
-  constructor(doc = {}) {
-    validateModel(this.constructor);
-    // modelRegistery.set(this.constructor.name, this.constructor); // TODO: mapping relationships
-
-    this.doc = doc;
-    this.doc[this.primaryKey] = this.doc[this.primaryKey] || puid.generate();
-  }
-
-  get primaryKey() {
-    return this.constructor.primaryKey || 'id';
-  }
-
-  async save() {
-    const schema = this.constructor.jsonSchema;
-
-    if (schema) {
-      const validate = ajv.compile(schema);
-      const valid = validate(this.doc);
-      if (!valid) {
-        // console.error(validate.errors); // TODO: proper error logger
-        throw new Error(
-          `document '${validate.errors[0].dataPath.replace(/^\./, '')}' ${
-            validate.errors[0].message
-          }`
-        );
-      }
-    }
-
-    await knex(this.constructor.tableName).insert(this.doc);
-
-    return knex(this.constructor.tableName)
-      .where({ [this.primaryKey]: this.doc[this.primaryKey] })
-      .first();
-  }
-
-  static get primaryKey() {
-    return 'id';
-  }
-
-  static query() {
-    return knex(this.tableName);
-  }
-
-  static async byId(id) {
-    return this.query()
-      .where({ [this.primaryKey]: id })
-      .first();
+class CustomModel extends BaseModel {
+  static onCreate(doc) {
+    doc[this.primaryKey] = doc[this.primaryKey] || puid.generate();
   }
 }
+
+export default CustomModel;

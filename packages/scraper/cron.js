688ebc593381076f15ebb91668ddd65ac4cef12b
Joe Fleming <joe.fleming@gmail.com>
2020-02-10 19:04:03 -0700

    fix: handle kill signals in scraper

--- packages/scraper/cron.js
+++ packages/scraper/cron.js
@@ -3,7 +3,21 @@ var debug = require('phxtech-debug')('cron');
 var saveEvents = require('.');
 var CronJob = cron.CronJob;
 
+function shutdown() {
+  // clean up your resources and exit
+  process.exit();
+};
+
+process.on('SIGINT', function onSigint() {
+  shutdown();
+});
+
+process.on('SIGTERM', function onSigterm() {
+  shutdown();
+});
+
 debug('Cron intialized');
+
 return new CronJob('0 17 6,11,17 * * *', function () {
   debug('Cron executing');
   saveEvents();
